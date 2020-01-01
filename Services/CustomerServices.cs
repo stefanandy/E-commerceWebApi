b8be6e5ba51b96d86669c28b6d65e144579711d5
stefanandy <31569796+stefanandy@users.noreply.github.com>
2020-01-01 03:28:26 +0200

    v0.2
    Product and customer services added

---
+++ Services/CustomerServices.cs
@@ -0,0 +1,80 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TuringEcommerce.Models;
+using TuringEcommerce.Services.Interfaces;
+
+namespace TuringEcommerce.Services
+{
+    public class CustomerServices:ICustomerServices
+    {
+        private readonly TuringContext _context;
+
+        public CustomerServices(TuringContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Customer> CreateCustomer(string name, string email, string password)
+        {
+            //TODO:Password hasher
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<Customer> UpdateCustomer(string email, string adress1, string adress2, string city, string region, string postalCode,
+            string country, int shippingRegionId)
+        {
+            var customer = await _context.Customer.FirstOrDefaultAsync(_customer => _customer.Email == email);
+
+            customer.Address1 = adress1;
+            customer.Address2 = adress2;
+            customer.City = city;
+            customer.Region = region;
+            customer.PostalCode = postalCode;
+            customer.Country = country;
+            customer.ShippingRegionId = shippingRegionId;
+
+            _context.Entry(customer).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return customer;
+        }
+
+        public async Task<Customer> UpdateCustomerPassword(string customerEmail, string newEmail, string name, string password, string dayPhone,
+            string evePhone, string mobPhone)
+        {
+            //TODO:Password hasher
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<Customer> UpdateCreditCard(string email, string creditCard)
+        {
+            var customer = await _context.Customer.FirstOrDefaultAsync(c => c.Email == email);
+            customer.CreditCard = creditCard;
+            _context.Entry(customer).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return customer;
+        }
+
+        public async Task<Customer> GetCustomerByEmail(string email)
+        {
+            return await _context.Customer.AsNoTracking().FirstOrDefaultAsync(customer => customer.Email == email);
+        }
+
+        public async Task<bool> EmailExist(string email)
+        {
+            var customer = await _context.Customer.FirstOrDefaultAsync(c => c.Email == email);
+            if (customer == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool EmailValid(string email)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- Services/Interfaces/IProductsServices.cs
+++ Services/Interfaces/IProductsServices.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using AspNetCore.PaginatedList;
+using PagedList;
+using PagedList.EntityFramework;
 using TuringEcommerce.Models;
 
 namespace TuringEcommerce.Services.Interfaces
@@ -9,19 +11,21 @@ namespace TuringEcommerce.Services.Interfaces
     public interface IProductsServices
     {
         public Task<IEnumerable<Product>> GetAllProducts();
-        public Task<Product> GetProductById();
+        public Task<Product> GetProductById(int id);
 
-        public Task<PaginatedList<Product>> GetProduct(string queryString, string allWords, int page, int limit,
+        public Task<IPagedList<Product>> GetProduct(string queryString, string allWords, int page, int limit,
             int descriptionLength);
         
-        public Task<PaginatedList<Product>> GetAllProductsInCategory(int page, int limit, int descriptionLength,
+        public Task<IPagedList<Product>> GetAllProductsInCategory(int page, int limit, int descriptionLength,
             int id);
 
-        public Task<PaginatedList<Product>> GetAllProductsInDepartament(int page, int limit, int descriptionLength,
+        public Task<IPagedList<Product>> GetAllProductsInDepartament(int page, int limit, int descriptionLength,
             int id);
 
-        public Task<IEnumerable<Review>> GetProductReviews(int id);
+        public Task<IEnumerable> GetProductLocations(int id);
+        public Task<ProductDetail> GetProductDetails(int id);
+        public Task<IEnumerable> GetProductReviews(int id);
 
-        public Task PostProductReview(int id);
+        public Task PostProductReview(string customerEmail, int id, string review, short rating);
     }
 }
