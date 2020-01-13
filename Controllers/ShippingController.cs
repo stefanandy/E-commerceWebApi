4075c5a1fe99a5e55b6baf216a0065b0157ee3d8
stefanandy <31569796+stefanandy@users.noreply.github.com>
2020-01-13 03:37:39 +0200

    v0.3
    Stripe, shipping and tax controllers added.

--- Controllers/ShippingController.cs
+++ Controllers/ShippingController.cs
@@ -1,6 +1,50 @@
-﻿namespace TuringEcommerce.Controllers
+﻿using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TuringEcommerce.Services.Interfaces;
+
+namespace TuringEcommerce.Controllers
 {
-    public class ShippingController
+    [ApiController]
+    [Route("shipping")]
+    public class ShippingController : ControllerBase
     {
+        private readonly IShippingServices _services;
+
+        public ShippingController(IShippingServices services)
+        {
+            _services = services;
+        }
+
+        [HttpGet("regions")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable>> GetRegions()
+        {
+            var regions = await _services.GetAllShipping();
+            if (!regions.Any())
+            {
+                return NotFound();
+            }
+
+            return Ok(regions);
+        }
+
+        [HttpGet("regions/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> GetById(int id)
+        {
+            var region = await _services.GetShippingById(id);
+            if (region==null)
+            {
+                return NotFound();
+            }
+
+            return Ok(region);
+        }
+
     }
 }
